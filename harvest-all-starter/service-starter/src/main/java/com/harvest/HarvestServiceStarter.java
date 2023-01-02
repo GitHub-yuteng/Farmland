144c9c7e6011b88f9ba7efb002d0fd24e426afe0
Alodi <995689575@qq.com>
2023-01-02 13:28:31 +0800

    Project add boot starter.

---
+++ harvest-all-starter/service-starter/src/main/java/com/harvest/HarvestServiceStarter.java
@@ -0,0 +1,21 @@
+package com.harvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author: Alodi
+ * @Date: 2023/1/1 8:59 PM
+ * @Description: TODO
+ **/
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.harvest")
+@SpringBootApplication(scanBasePackages = "com.harvest")
+public class HarvestServiceStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarvestServiceStarter.class, args);
+    }
+}
