68e101fb28294dd4be33da908a78275482d50ba0
Alodi <995689575@qq.com>
2023-02-02 20:00:52 +0800

    feat(Logistics): fix pom.

--- harvest-basic-web/src/main/java/com/harvest/basic/web/controller/config/WebConfigController.java
+++ harvest-basic-web/src/main/java/com/harvest/basic/web/controller/config/WebConfigController.java
@@ -3,7 +3,7 @@ package com.harvest.basic.web.controller.config;
 import com.harvest.basic.client.config.WebConfigClient;
 import com.harvest.basic.repository.enums.WebConfigEnum;
 import com.harvest.core.domain.ResponseResult;
-import com.harvest.core.path.HarvestOmsPath;
+import com.harvest.core.path.HarvestBasicPath;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2023/1/8 11:34 PM
  * @Description: TODO
  **/
-@Api(value = "订单丰富查询", tags = "订单查询")
+@Api(value = "前端自定义配置服务", tags = "前端自定义配置服务")
 @RestController
-@RequestMapping(value = HarvestOmsPath.BasicPath.WEB_CONFIG)
+@RequestMapping(value = HarvestBasicPath.BasicPath.WEB_CONFIG)
 public class WebConfigController {
 
     private WebConfigClient webConfigClient;
