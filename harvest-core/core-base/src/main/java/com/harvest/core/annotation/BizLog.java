19a35f783def4c33ecedd9caae71bab140df1058
Alodi <995689575@qq.com>
2023-03-15 14:00:42 +0800

    feat: add note.

--- harvest-core/core-base/src/main/java/com/harvest/core/annotation/BizLog.java
+++ harvest-core/core-base/src/main/java/com/harvest/core/annotation/BizLog.java
@@ -5,7 +5,8 @@ import java.lang.annotation.*;
 /**
  * @Author: Alodi
  * @Date: 2023/3/13 2:34 PM
- * @Description: 业务日志注解
+ * @Description: 业务日志注解 配合 BizLogUtils 使用
+ * @link {@link com.harvest.core.service.utils.BizLogUtils}
  **/
 @Documented
 @Target(ElementType.METHOD)
